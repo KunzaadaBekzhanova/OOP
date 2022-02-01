66042627e674aea0d9ebd05d28d771f0f1321ab3
SunflowerKun <dream26322gmail.com>
2022-02-01 11:41:02 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Triangle triangle = new Triangle();
+    triangle.a = 5;
+    triangle.b = 4;
+    triangle.h = 10;
+    triangle.area();
+    }
+}
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Triangle {
+    int a;
+    int b;
+    int h;
+    public void area(){
+        System.out.println((b*h)/2);
+    }
+}
